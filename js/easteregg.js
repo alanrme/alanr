a94ec9ae648e56a36f50a30eb6bbac8c0c877626
Alan <alanrd772@gmail.com>
2023-07-02 16:35:30 +0400

    Modify and optimize easteregg

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,6 +1,4 @@
 function showEgg(egg) {
-    // var for compatibility
-    var audio = new Audio("./audio/easteregg.mp3")
     //audio.play()
     egg.style.display = "block"
     setTimeout(function(){
@@ -11,7 +9,8 @@ function showEgg(egg) {
 function hideEgg(egg) {
     egg.classList.remove("show")
     egg.addEventListener("transitionend", () => {
-        if (!egg.classList.contains("show")) // so this only fires when fading out
+        // so this only fires when fading out
+        if (!egg.classList.contains("show"))
             egg.style.display = "none"
     })
 }
@@ -24,10 +23,6 @@ ready(() => {
             hideEgg(egg)
         })
     })
-    _("#loader-enable").addEventListener("click", () => { // when loader button clicked
-        _("#loader-bg").style.display = "block"
-        _("#loader-bg").style.opacity = 1
-    })
 
     //CLICK-TRIGGER EASTEREGG
     _("#trigger").addEventListener("click", () => { // when trigger clicked
@@ -36,40 +31,28 @@ ready(() => {
 
 
     //KONAMI CODE
-    var konamikeys = [38,38,40,40,37,39,37,39,66,65], 
-    started = false, 
+    var konamikeys = [38,38,40,40,37,39,37,39,66,65],
     count = 0
     
     document.addEventListener('keydown', e => {
         var reset = function() {
-            started = false 
             count = 0
         }
         
-        // begin watching if first key in sequence was pressed
-        if(!started){
-            if(e.keyCode == konamikeys[0]){
-                started = true
-            }
+        if (konamikeys[count] == e.keyCode){
+            count++
+        } else {
+            // incorrect key, reset
+            reset()
         }
-        
-        // if sequence was started, start checking keypresses
-        if (started){
-            if (konamikeys[count] == e.keyCode){
-                count++
-            } else {
-                // incorrect key, reset
-                reset()
-            }
 
-            if (count == konamikeys.length){ // full code entered
-                if (egg.style.display === "none") { // if diplay none fade in easteregg
-                    showEgg(egg)
-                } else {
-                    hideEgg(egg)
-                }
-                reset()
+        if (count == konamikeys.length){ // full code entered
+            if (egg.style.display === "none") { // if diplay none fade in easteregg
+                showEgg(egg)
+            } else {
+                hideEgg(egg)
             }
+            reset()
         }
     })
 })
